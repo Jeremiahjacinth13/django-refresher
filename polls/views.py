a76b2fa6a292f14098a8c9573e16de4494094940
Jeremiah Lena <jeremiahlena13@gmail.com>
2022-01-24 15:30:49 +0100

    rearranged folder structure

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,12 @@ def index(request):
     return render(request, 'polls/index.html', context = context)
 
 def detail(request, question_id):
-    return HttpResponse('You are looking at question %s.' % question_id)
+    try:
+        question = Question.objects.get(id = question_id)
+    except Question.DoesNotExist:
+        return HttpResponse('Question %s does not exist' % question_id)
+    return render(request, 'polls/details.html', { 'question': question })
+    
 
 def results(request, question_id):
     return HttpResponse('You are looking at the results of question %s.' % question_id)
